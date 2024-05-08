411836a8237a05b7ce4d9926d66c174028e9f542
Sofffia <vortelxl@gmail.com>
2024-05-08 14:08:20 +0300

    лаб5

--- lab5/script5.js
+++ lab5/script5.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const Btn1 = document.getElementById('theme');
-    const Btn2 = document.getElementById('desktop');
-    const Btn3 = document.getElementById('plate');
-    const Btn4 = document.getElementById('phone');
+    const Btn2 = document.getElementById('task1');
+    const Btn3 = document.getElementById('task2');
+    const Btn4 = document.getElementById('task3');
     const Btn5 = document.getElementById('result');
 
 
@@ -11,20 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     Btn2.addEventListener('click', function() {
-        infoBlock.innerHTML = "<img src='img/lab3/2.png'><br>" +
-            "<img src='img/lab3/3.png'><br>" +
-            "<img src='img/lab3/4.png'><br>" +
-            "<img src='img/lab3/5.png'><br>" +
-            "<img src='img/lab4/1.png'><br>" +
-            "<img src='img/lab4/2.png'><br>" +
-            "<img src='img/lab4/3.png'><br>" +
-            "<img src='img/lab4/4.png'><br>" +
-            "<img src='img/lab4/5.png'><br>" +
-            "<img src='img/lab4/6.png'><br>" +
-            "<img src='img/lab4/7.png'><br>" +
-            "<img src='img/lab4/8.png'><br>" +
-            "<img src='img/lab4/9.png'><br>" +
-            "<img src='img/lab4/10.png'><br>";
+        infoBlock.innerHTML =
+            "<p>1.Створити одновимірний масив, кількість елементів якого задана користувачем. Знайти максимальний серед парних елементів та мінімальний серед елементів з парними індексами. В масиві поміняти місцями знайдені максимальний та мінімальний елементи, надрукувати вхідний та вихідний масиви.\n" +
+            "2. Виконати сортування вихідного масиву за зростанням методом вставки.\n" +
+            "Застосувати функції</p>" +
+            "<img src='../img/lab5/1.png'><br>" +
+            "<img src='../img/lab5/2.png'><br>" +
+            "<img src='../img/lab5/3.png'><br>" +
+            "<img src='../img/lab5/4.png'><br>";
     });
 
     Btn1.addEventListener('click', function() {
@@ -36,49 +30,25 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     Btn3.addEventListener('click', function() {
-        infoBlock.innerHTML = "<img src='img/lab3/2.png'><br>" +
-            "<img src='img/lab3/3.png'><br>" +
-            "<img src='img/lab3/4.png'><br>" +
-            "<img src='img/lab3/5.png'><br>" +
-            "<img src='img/lab4/11.png'><br>" +
-            "<img src='img/lab4/12.png'><br>" +
-            "<img src='img/lab4/13.png'><br>" +
-            "<img src='img/lab4/14.png'><br>" +
-            "<img src='img/lab4/15.png'><br>" +
-            "<img src='img/lab4/16.png'><br>" +
-            "<img src='img/lab4/17.png'><br>" +
-            "<img src='img/lab4/18.png'><br>" +
-            "<img src='img/lab4/19.png'><br>";
+        infoBlock.innerHTML =
+            "<p>Валідація HTML-форми реєстрації користувача.Забезпечити перевірку елементів HTML-форми таких видів: ціле число, дійсне число, дата у форматі DD.MM.YYYY, однаковість даних у полях введення паролю (основне та перевірочне). Крім того, забезпечити перевірку введення обов‘язкових полів, помічених (*). " +"<img src='../img/lab5/5.png'><br>" +
+            "<img src='../img/lab5/6.png'><br>" +
+            "<img src='../img/lab5/7.png'><br>" +
+            "<img src='../img/lab5/8.png'><br>" +
+            "<img src='../img/lab5/9.png'><br>" +
+            "<img src='../img/lab5/10.png'><br>" +
+            "<img src='../img/lab5/11.png'><br>";
     });
     Btn4.addEventListener('click', function() {
-        infoBlock.innerHTML = "<img src='img/lab3/2.png'><br>" +
-            "<img src='img/lab3/3.png'><br>" +
-            "<img src='img/lab3/4.png'><br>" +
-            "<img src='img/lab3/5.png'><br>" +
-            "<img src='img/lab4/20.png'><br>" +
-            "<img src='img/lab4/21.png'><br>" +
-            "<img src='img/lab4/22.png'><br>" +
-            "<img src='img/lab4/23.png'><br>" +
-            "<img src='img/lab4/24.png'><br>" +
-            "<img src='img/lab4/25.png'><br>" +
-            "<img src='img/lab4/26.png'><br>";
+        infoBlock.innerHTML =
+            "<img src='../img/lab5/12.png'><br>" +
+            "<img src='../img/lab5/13.png'><br>" +
+            "<img src='../img/lab5/14.png'><br>" +
+            "<img src='../img/lab5/15.png'><br>";
     });
 
     Btn5.addEventListener('click', function() {
         infoBlock.innerHTML = "<h1>Висновки</h1>" +
-            "<p>У ході виконання цієї роботи ми досліджували та впроваджували концепції адаптивної верстки," +
-            " використовуючи медіа-запити, медіа-типи, медіа-функції, а також метатег viewport. Наша стратегія" +
-            " базувалася на принципі \"Mobile First\", що передбачає розробку сайту спочатку для мобільних пристроїв," +
-            " а потім для більш розширених пристроїв.\n" +
-            "\n" +
-            "У звітному HTML-документі ми надали пояснення щодо створення десктопної, планшетної та мобільної версій сайту. " +
-            "Для цього ми використали різні медіа-запити, які залежали від ширини екрану пристрою. Наприклад, для десктопної" +
-            " версії ми встановлювали ширину контейнера і розміщували контент і бокову панель поруч один з одним. " +
-            "У планшетній версії ми змінювали максимальну ширину контейнера та розміщували контент і бокову панель " +
-            "на всю доступну ширину екрану. А в мобільній версії ми змінювали спосіб відображення контенту, щоб він " +
-            "був легко читабельним на вузьких екранах.\n" +
-            "\n" +
-            "Ця робота дозволила нам створити відзивчивий та користувацьки-орієнтований дизайн, який працює ефективно " +
-            "на різних пристроях і забезпечує зручний досвід для користувачів незалежно від їхнього пристрою.</p><br>";
+            "<p>У результаті виконання роботи ми здобули практичні навички у використанні масивів у JavaScript і створенні програм з використанням цієї мови програмування. Ми освоїли структуру масивів та їхні можливості, які дозволяють ефективно зберігати та обробляти дані. Крім того, зрозуміли, як JavaScript може використовуватися для динамічної зміни вмісту HTML-сторінок та взаємодії з користувачем. Реалізація програм за допомогою JavaScript дала нам практичний досвід у вирішенні завдань веб-розробки. Практична робота є важливим кроком у вивченні будь-якої програмної мови, оскільки дозволяє закріпити теоретичні знання та набути практичних навичок. В цілому, виконання завдань дало можливість успішно досягти поставленої мети і отримати практичний досвід у роботі з JavaScript.</p><br>";
     });
 });
